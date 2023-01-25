426ef92a0ff494c878fc1f8f03fca87f3323d30b
Abram <israelvictory87@gmail.com>
2023-01-25 18:03:50 +0100

    Feat: created main module

---
+++ sms/main.py
@@ -0,0 +1,16 @@
+# Uvicorn Imports
+import uvicorn
+
+# FastAPI Imports
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="SMS Reminder System",
+    description="An SMS reminder system that allows users to text a specific number to set reminders for themselves.",
+    verison=1.0
+)
+
+
+if __name__ == "__main__":
+    uvicorn.run("sms.main:app", host="0.0.0.0", port=3030)
