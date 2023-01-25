0ffb7f86ea75b5543bdced6bdc2288923ca8fe94
Abram <israelvictory87@gmail.com>
2023-01-25 18:25:24 +0100

    Update: created functions to connect and disconnect database on startup/disconnect app event

--- sms_reminder/main.py
+++ sms_reminder/main.py
@@ -4,12 +4,25 @@ import uvicorn
 # FastAPI Imports
 from fastapi import FastAPI
 
+# Own Imports
+from sms_reminder.config.database import db_connect
+
 
 app = FastAPI(
     title="SMS Reminder System",
     description="An SMS reminder system that allows users to text a specific number to set reminders for themselves.",
-    verison=1.0
+    verison=1.0,
 )
+
+
+@app.on_event("startup")
+async def startup():
+    await db_connect.connect()
+
+
+@app.on_event("shutdown")
+async def disconnect():
+    await db_connect.disconnect()
 
 
 if __name__ == "__main__":
