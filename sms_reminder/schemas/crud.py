05ccbfbdcb0b396e65aee36a92352f888e5067f1
Abram <israelvictory87@gmail.com>
2023-01-27 14:51:00 +0100

    Update: modified phone_number field

--- sms_reminder/schemas/crud.py
+++ sms_reminder/schemas/crud.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel, Field
 
 class BaseReminderSchema(BaseModel):
     phone_number: str = Field(
-        description="What's your phone number? Ensure it is valid."
+        description="What's your phone number? Ensure you include your country code and is valid. E.g 234xxxxxxxxxx"
     )
     message: str = Field(
         description="What message do you want to remind yourself with? E.g Time to go to the gym!"
@@ -15,7 +15,7 @@ class BaseReminderSchema(BaseModel):
         description="When should I send this message to you?",
         default=datetime.now(tz=pytz.timezone("Africa/Lagos")),
     )
-    
+
 
 class CreateReminderSchema(BaseReminderSchema):
     pass
